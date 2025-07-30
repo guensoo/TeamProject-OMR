1ab92274ca7c02e1473b2690e4655fba21f84bfe
isak9975 <isak9975@gmail.com>
2025-07-30 17:51:01 +0900

    2025.07.30

--- BackEnd/teamProject.OMR/src/main/java/OMR/teamProject/OMR/FAQ/Controller/FAQController.java
+++ BackEnd/teamProject.OMR/src/main/java/OMR/teamProject/OMR/FAQ/Controller/FAQController.java
@@ -1,8 +1,15 @@
 package OMR.teamProject.OMR.FAQ.Controller;
 
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +18,36 @@ import OMR.teamProject.OMR.FAQ.Service.FAQService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/faqs")
+@RequestMapping("/api/faq")
 @RequiredArgsConstructor
 public class FAQController {
     private final FAQService faqService;
 
+    //C
+    @PostMapping
+    public ResponseEntity<?> write(@RequestBody FAQDto dto){
+    	List<FAQDto> result =faqService.write(dto);
+    	return ResponseEntity.ok().body(result);
+    }
+    
+    //R
     @GetMapping
-    public List<FAQDto> getAll() {
-        return faqService.getAllFaqs();
+    public ResponseEntity<?> findAll() {
+    	List<FAQDto> result =faqService.findAll();
+        return ResponseEntity.ok().body(result);
+    }
+    
+    //U
+    @PutMapping
+    public ResponseEntity<?> update(@RequestBody FAQDto dto){
+    	boolean result =faqService.update(dto);
+        return ResponseEntity.ok().body(Map.of("result",result));
+    }
+    
+    //D
+    @DeleteMapping("/id")
+    public ResponseEntity<?> delete(@PathVariable long id){
+    	boolean result =faqService.delete(id);
+    	return ResponseEntity.ok().body(Map.of("Result",result));
     }
 }
