c5829e79ae593e27f682f7141ee36e0b39a2a46b
isak9975 <isak9975@gmail.com>
2025-07-31 09:56:26 +0900

    db연결

--- BackEnd/teamProject.OMR/src/main/java/OMR/teamProject/OMR/User/Repository/UserRepository.java
+++ BackEnd/teamProject.OMR/src/main/java/OMR/teamProject/OMR/User/Repository/UserRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import OMR.teamProject.OMR.User.Entity.UserEntity;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    Optional<UserEntity> findByUsername(String username);
+//    Optional<UserEntity> findByUsername(String username);
 }
